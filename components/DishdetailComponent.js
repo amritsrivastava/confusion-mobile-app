2fb2ec5940c7058fbc44a12063ad2f4304f2711c
amritsrivastava <srivastava.amrit1999@gmail.com>
2020-07-20 22:31:54 +0530

    Add comments form

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList } from 'react-native';
-import { Card, Icon } from 'react-native-elements';
+import {
+  Text,
+  View,
+  ScrollView,
+  FlatList,
+  Modal,
+  Button,
+  StyleSheet,
+} from 'react-native';
+import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { postFavorite } from '../redux/ActionCreators';
+import { postComment } from '../redux/ActionCreators';
 
 const mapStateToProps = (state) => {
   return {
@@ -15,6 +24,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => ({
   postFavorite: (dishId) => dispatch(postFavorite(dishId)),
+  postComment: (dishId, rating, author, comment) =>
+    dispatch(postComment(dishId, rating, author, comment)),
 });
 
 function RenderDish(props) {
@@ -24,16 +35,26 @@ function RenderDish(props) {
     return (
       <Card featuredSubtitle={dish.name} image={{ uri: baseUrl + dish.image }}>
         <Text style={{ margin: 10 }}>{dish.description}</Text>
-        <Icon
-          raised
-          reverse
-          name={props.favorite ? 'heart' : 'heart-o'}
-          type="font-awesome"
-          color="#f50"
-          onPress={() =>
-            props.favorite ? console.log('Already favorite') : props.onPress()
-          }
-        />
+        <View style={styles.iconContainer}>
+          <Icon
+            raised
+            reverse
+            name={props.favorite ? 'heart' : 'heart-o'}
+            type="font-awesome"
+            color="#f50"
+            onPress={() =>
+              props.favorite ? console.log('Already favorite') : props.onPress()
+            }
+          />
+          <Icon
+            raised
+            reverse
+            name="pencil"
+            type="font-awesome"
+            color="#512DA8"
+            onPress={() => props.toggleModal()}
+          />
+        </View>
       </Card>
     );
   } else {
@@ -68,6 +89,41 @@ function RenderComments(props) {
 }
 
 class Dishdetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rating: 4,
+      author: '',
+      comment: '',
+      showModal: false,
+    };
+
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  handleComment() {
+    this.resetForm();
+    this.toggleModal();
+    this.props.postComment(
+      this.props.navigation.getParam('dishId', ''),
+      this.state.rating,
+      this.state.author,
+      this.state.comment
+    );
+  }
+
+  toggleModal() {
+    this.setState({ showModal: !this.state.showModal });
+  }
+
+  resetForm() {
+    this.setState({
+      rating: 4,
+      author: '',
+      comment: '',
+    });
+  }
+
   markFavorite(dishId) {
     this.props.postFavorite(dishId);
   }
@@ -84,15 +140,89 @@ class Dishdetail extends Component {
           dish={this.props.dishes.dishes[+dishId]}
           favorite={this.props.favorites.some((el) => el === dishId)}
           onPress={() => this.markFavorite(dishId)}
+          toggleModal={this.toggleModal}
         />
         <RenderComments
           comments={this.props.comments.comments.filter(
             (comment) => comment.dishId === dishId
           )}
         />
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.showModal}
+          onDismiss={() => {
+            this.toggleModal();
+            this.resetForm();
+          }}
+          onRequestClose={() => {
+            this.toggleModal();
+            this.resetForm();
+          }}
+        >
+          <View style={styles.modal}>
+            <Rating
+              showRating
+              startingValue={this.state.rating}
+              onFinishRating={(rating) => this.setState({ rating: rating })}
+            />
+            <Input
+              containerStyle={{ marginTop: 10, marginBottom: 10 }}
+              placeholder="Author"
+              leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+              leftIconContainerStyle={{ marginRight: 10 }}
+              value={this.state.author}
+              onChangeText={(author) => this.setState({ author: author })}
+            />
+            <Input
+              containerStyle={{ marginTop: 10, marginBottom: 10 }}
+              placeholder="Comment"
+              leftIcon={{ type: 'font-awesome', name: 'comment-o' }}
+              leftIconContainerStyle={{ marginRight: 10 }}
+              value={this.state.comment}
+              onChangeText={(comment) => this.setState({ comment: comment })}
+            />
+            <View style={styles.buttonContainer}>
+              <Button
+                onPress={() => {
+                  this.handleComment();
+                }}
+                color="#512DA8"
+                title="SUBMIT"
+              />
+            </View>
+            <View style={styles.buttonContainer}>
+              <Button
+                onPress={() => {
+                  this.toggleModal();
+                  this.resetForm();
+                }}
+                color="grey"
+                title="CANCEL"
+              />
+            </View>
+          </View>
+        </Modal>
       </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  modal: {
+    justifyContent: 'center',
+    margin: 20,
+  },
+  buttonContainer: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  iconContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dishdetail);
--- redux/comments.js
+++ redux/comments.js
@@ -23,6 +23,12 @@ export const comments = (
         comments: [],
       };
 
+    case ActionTypes.ADD_COMMENT:
+      var comment = action.payload;
+      comment.id = state.comments.length;
+      comment.date = new Date().toISOString();
+      return { ...state, comments: state.comments.concat(comment) };
+
     default:
       return state;
   }

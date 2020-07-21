748eb9109197ba716b6a89feffa5f56b4cd119bd
amritsrivastava <srivastava.amrit1999@gmail.com>
2020-07-21 09:56:21 +0530

    delete favorite

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -183,3 +183,8 @@ export const postComment = (dishId, rating, author, comment) => (dispatch) => {
     dispatch(addComment(newComment));
   }, 2000);
 };
+
+export const deleteFavorite = (dishId) => ({
+  type: ActionTypes.DELETE_FAVORITE,
+  payload: dishId,
+});
--- redux/favorites.js
+++ redux/favorites.js
@@ -5,6 +5,12 @@ export const favorites = (state = [], action) => {
     case ActionTypes.ADD_FAVORITE:
       if (state.some((el) => el === action.payload)) return state;
       else return state.concat(action.payload);
+
+    case ActionTypes.DELETE_FAVORITE:
+      const newState = state.filter((favorite) => favorite !== action.payload);
+      console.log('Hello bud');
+      return newState;
+
     default:
       return state;
   }
